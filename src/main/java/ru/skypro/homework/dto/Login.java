6de7d3ccaa6debb7754124213ca43000c5670982
Admin <git@github.com:Luchnik1984/UsingVariablesApplication.git>
2025-12-15 11:43:54 +0300

    US1.1 commit

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -1,10 +1,20 @@
 package ru.skypro.homework.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class Login {
 
+    @Schema(description = "Логин пользователя",
+            example = "user@example.com",
+            minLength = 4,
+            maxLength = 32)
     private String username;
+
+    @Schema(description = "Пароль пользователя",
+            example = "password123",
+            minLength = 8,
+            maxLength = 16)
     private String password;
 }
